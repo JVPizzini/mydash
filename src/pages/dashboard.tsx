fd55277536da59776a5211b74095ed58cace2285
JVPizzini <joaovitorpizzini@hotmail.com>
2021-10-07 23:27:36 -0300

    header + sidebar + page dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,20 @@
+
+//chakra-ui
+import { Flex } from '@chakra-ui/react';
+
+// components
+import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
+
+export default function DashBoard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar/>
+
+      </Flex>
+    </Flex>
+  )
+}
+
